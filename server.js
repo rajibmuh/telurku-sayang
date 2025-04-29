8e0eb529f35f8777453c71bf5ec250275770beaa
Rajib <rajmuh186@gmail.com>
2025-04-29 23:31:45 +0700

    update semua perubahan terbaru

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-// Layani semua file statis dari folder "public"
 app.use(express.static('public'));
 
 app.listen(port, () => {
